e0de0e2b7283e18875dcc846494be57cbf1edfc4
SamuelFoo <fooenzesamuel@gmail.com>
2024-10-06 17:14:54 +0800

    Add hysteresis

--- helper.py
+++ helper.py
@@ -1,7 +1,8 @@
 import numpy as np
-from constants import *
 from numpy.typing import ArrayLike
 from scipy.integrate import solve_ivp
+
+from constants import *
 
 
 def kelvin(celsius: ArrayLike):
@@ -40,19 +41,30 @@ def albedo(temperature: np.ndarray):
     return albedo
 
 
-def beta(T: ArrayLike):
+def beta(D_f: float, T_opt: float, T: ArrayLike):
     """Daisy's growth
 
     Args:
-        T (float): Daisy's local temperature
+        D_f (float): Daisy Death Factor.
+        T_opt (float): Optimal temperature.
+        T (float): Daisy's local temperature.
 
     Returns:
         float: daisy's growth
     """
-    return 1 - 0.003265 * (295.5 - T) ** 2
+    return 1 - D_f * (T_opt - T) ** 2
 
 
-def solve(L, A_w, A_b):
+def _A_g(A_w: float, A_b: float):
+    return 1 - A_w - A_b
+
+
+def _T_p(A_w, A_b, L):
+    alpha_p = A_w * alpha_w + A_b * alpha_b + _A_g(A_w=A_w, A_b=A_b) * alpha_g
+    return (L * S_0 / sigma * (1 - alpha_p)) ** (1 / 4)
+
+
+def __solve(L, A_w, A_b, D_f, T_opt, dense_output=False):
     local_T = lambda alpha_p, alpha_i, T_p: (
         R * L * S_0 / sigma * (alpha_p - alpha_i) + T_p**4
     ) ** (1 / 4)
@@ -66,19 +78,54 @@ def solve(L, A_w, A_b):
         T_w = local_T(alpha_p, alpha_i=alpha_w, T_p=T_p)
         T_b = local_T(alpha_p, alpha_i=alpha_b, T_p=T_p)
 
-        beta_w = beta(T_w)
-        beta_b = beta(T_b)
+        beta_w = beta(D_f=D_f, T_opt=T_opt, T=T_w)
+        beta_b = beta(D_f=D_f, T_opt=T_opt, T=T_b)
 
         return [A_w * (x * beta_w - gamma), A_b * (x * beta_b - gamma)]
 
     t_f = 50
-    sol = solve_ivp(f, [0, t_f], y0=[A_w, A_b])
+    sol = solve_ivp(f, [0, t_f], y0=[A_w, A_b], dense_output=dense_output)
+    return sol
 
-    A_w, A_b = sol.y
-    A_g = x = 1 - A_w - A_b
-    alpha_p = A_w * alpha_w + A_b * alpha_b + A_g * alpha_g
+
+def solve_dense(L, A_w, A_b, D_f, T_opt):
+    sol = __solve(L=L, A_w=A_w, A_b=A_b, D_f=D_f, T_opt=T_opt, dense_output=True)
+    sol_fn = sol.sol
+    t_f = 50
+    ts = np.linspace(0, t_f, 1000)
+    A_w, A_b = sol_fn(ts)
+
+    alpha_p = A_w * alpha_w + A_b * alpha_b + _A_g(A_w=A_w, A_b=A_b) * alpha_g
+    assert np.all(alpha_p < 1)
     T_p = (L * S_0 / sigma * (1 - alpha_p)) ** (1 / 4)
-    return T_p[-1], max(A_w[-1], 0.01), max(A_b[-1], 0.01)
+    return ts, T_p, A_w, A_b
+
+
+def solve(L, A_w, A_b, D_f, T_opt):
+    sol = __solve(L=L, A_w=A_w, A_b=A_b, D_f=D_f, T_opt=T_opt)
+    A_w, A_b = sol.y
+    alpha_p = A_w * alpha_w + A_b * alpha_b + _A_g(A_w=A_w, A_b=A_b) * alpha_g
+    T_p = (L * S_0 / sigma * (1 - alpha_p)) ** (1 / 4)
+    return sol.t, T_p, A_w, A_b
+
+
+def solve_varying_L_quasi_static(Ls, A_w0, A_b0, D_f, T_opt):
+    T_ps = []
+    A_ws, A_bs = [], []
+    A_w = A_w0
+    A_b = A_b0
+
+    for L in Ls:
+        _, T_p_against_time, A_w_against_time, A_b_against_time = solve(
+            L, A_w=A_w, A_b=A_b, D_f=D_f, T_opt=T_opt
+        )
+        A_w = max(A_w_against_time[-1], 0.01)
+        A_b = max(A_b_against_time[-1], 0.01)
+        A_ws.append(A_w)
+        A_bs.append(A_b)
+        T_ps.append(T_p_against_time[-1])
+
+    return T_ps, A_ws, A_bs
 
 
 def years_to_seconds(num_years: float):
---
+++ hysteresis.py
@@ -0,0 +1,89 @@
+import numpy as np
+from scipy.misc import derivative
+from scipy.optimize import minimize
+
+from constants import S_0, R, alpha_b, alpha_g, alpha_w, gamma, sigma
+from helper import kelvin
+
+
+def vb(Ab, Aw, L, D_f=0.003265, T_opt=kelvin(22.5)):
+    """
+    dA/dt of Black Daisy
+    """
+    alpha_p = Aw * alpha_w + Ab * alpha_b + (1 - Aw - Ab) * alpha_g
+    T_p = (L * (S_0 / sigma) * (1 - alpha_p)) ** 0.25
+    T_b = (R * L * (S_0 / sigma) * (alpha_p - alpha_b) + (T_p**4)) ** 0.25
+    beta_b = 1 - (D_f * (T_opt - T_b) ** 2)
+    return Ab * ((1 - Ab - Aw) * beta_b - gamma)
+
+
+def vw(Ab, Aw, L, D_f=0.003265, T_opt=kelvin(22.5)):
+    """
+    dA/dt of White Daisy
+    """
+    ap = Aw * alpha_w + Ab * alpha_b + (1 - Aw - Ab) * alpha_g
+    Te = (L * (S_0 / sigma) * (1 - ap)) ** 0.25
+    Tw = (R * L * (S_0 / sigma) * (ap - alpha_w) + (Te**4)) ** 0.25
+    bw = 1 - (D_f * (T_opt - Tw) ** 2)
+    return Aw * ((1 - Aw - Ab) * bw - gamma)
+
+
+def equilibrium(L):
+    def cost(A):
+        """
+        Auxillary Function for Cost (to be minimized)
+        """
+        Ab, Aw = A
+        return (vb(Ab, Aw, L=L)) ** 2 + (vw(Ab, Aw, L=L)) ** 2
+
+    # Root Finder
+    solutions = []
+    testvec = np.linspace(0, 1, num=21)
+    for Ab0 in testvec:
+        for Aw0 in testvec:
+            if (Ab0 + Aw0) <= 1:
+                A0 = np.array([Ab0, Aw0])
+                res = minimize(cost, A0, method="nelder-mead", options={"xatol": 1e-8})
+                sb = round(res.x[0], 5)
+                sw = round(res.x[1], 5)
+                if (
+                    ((sb + sw) <= 1)
+                    and (sb >= 0)
+                    and (sw >= 0)
+                    and (cost([sb, sw]) < 1e-7)
+                ):
+                    if [sb, sw] not in solutions:
+                        solutions.append([sb, sw])
+
+    def partial_derivative(func, var, point):
+        """
+        Auxillary Function for Partial Derivatives
+        """
+        args = point[:]
+
+        def wraps(x):
+            args[var] = x
+            return func(*args, L=L)
+
+        return derivative(wraps, point[var], dx=1e-5)
+
+    # Stability Finder
+    nature = []
+    for s in solutions:
+        eb, ew = s
+        j00 = partial_derivative(vb, var=0, point=[eb, ew])
+        j01 = partial_derivative(vb, var=1, point=[eb, ew])
+        j10 = partial_derivative(vw, var=0, point=[eb, ew])
+        j11 = partial_derivative(vw, var=1, point=[eb, ew])
+        jacobian = np.array([[j00, j01], [j10, j11]])
+        eigval, _ = np.linalg.eig(jacobian)
+        if eigval[0] < 0 and eigval[1] < 0:
+            nature.append("Stable")
+        else:
+            nature.append("Unstable")
+
+    solutions = np.array(solutions)
+    nature = np.array(nature)
+    stable_points = solutions[nature == "Stable"]
+    unstable_points = solutions[nature == "Unstable"]
+    return stable_points, unstable_points
